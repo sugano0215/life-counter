41039a8deed23bbcbafeab5af1b189990a3ff235
sugano_junpei <sugano_junpei@interprism.co.jp>
2016-10-19 20:52:25 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/example/sugano_junpei/life_counter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sugano_junpei.life_counter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
