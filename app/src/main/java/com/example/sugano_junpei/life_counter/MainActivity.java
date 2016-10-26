6c865cfadef07a27ed9d9a58f30495dad94a13cb
sugano_junpei <sugano_junpei@interprism.co.jp>
2016-10-26 20:53:35 +0900

    viewを仮置きしてみた

--- app/src/main/java/com/example/sugano_junpei/life_counter/MainActivity.java
+++ app/src/main/java/com/example/sugano_junpei/life_counter/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.sugano_junpei.life_counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button teamA, teamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teamA = (Button) findViewById(R.id.teamA);
+        teamB = (Button) findViewById(R.id.teamB);
     }
 }
